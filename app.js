c50d78269fd282b0c2c18f12f542b83990fab0eb
Yuki IGARASHI <me@bonprosoft.com>
2015-07-16 04:17:12 +0900

    :scream_cat:

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-/// <reference path="typings/jquery/jquery.d.ts"/>
-$('#rotate').on('click', function () {
-    $('#rotate').anim
-});
+/// <reference path="typings/jquery/jquery.d.ts"/>  
+$('#rotate').on('click', function () {  
+    $('#rotate').animate({ zIndex : 1 },{  
+        duration : 1000,  
+        step : function (value) {  
+            $(this).css({  
+                transform : 'rotate(' + (value * 360) + 'deg)'  
+            });  
+        },  
+        complete : function () {  
+            $('#rotate').css('zIndex', 0);  
+        }  
+    });  
+});  
